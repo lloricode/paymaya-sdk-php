240e284b835638d464c344a6a82146c483b5a897
Lloric Mayuga Garcia <lloricode@gmail.com>
2025-08-24 14:23:22 +0800

    Refactor: Remove redundant retries and error-handling traits from `PaymayaConnector`

--- src/PaymayaConnector.php
+++ src/PaymayaConnector.php
@@ -19,24 +19,16 @@ use Saloon\Http\Auth\TokenAuthenticator;
 use Saloon\Http\Connector;
 use Saloon\Http\PendingRequest;
 use Saloon\Traits\Plugins\AcceptsJson;
-use Saloon\Traits\Plugins\AlwaysThrowOnErrors;
 use Saloon\Traits\Plugins\HasTimeout;
 
 class PaymayaConnector extends Connector
 {
     use AcceptsJson;
-    use AlwaysThrowOnErrors;
     use HasTimeout;
 
     protected int $connectTimeout = 60;
 
     protected int $requestTimeout = 120;
-
-    public ?int $tries = 3;
-
-    public ?int $retryInterval = 500;
-
-    public ?bool $useExponentialBackoff = true;
 
     public function __construct(
         private readonly Environment $environment,
